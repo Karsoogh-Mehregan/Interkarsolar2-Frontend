07e1f6f66b32f0031b0f4281546bf5592a29ef6e
amoohashem <amoo.hashem.mehraban@gmail.com>
2021-02-13 13:21:25 +0330

    fix toast bug

--- src/redux/reducers/notifications.js
+++ src/redux/reducers/notifications.js
@@ -9,21 +9,37 @@ function notifications(state = initState, action) {
   switch (action.type) {
 
     case actionTypes.CREATE_ACCOUNT_SUCCESS:
-      toast.success('ورودت رو به کهکشان مهرگان خوش‌آمد میگم!');
-      return { ...state }
+      setTimeout(
+        () => {
+          toast.success('ورودت رو به کهکشان مهرگان خوش‌آمد میگم!');
+        }
+        , 0);
+      return { ...state };
 
     case actionTypes.LOGIN_SUCCESS:
-      toast.success('دوباره سلام!');
-      return { ...state }
+      setTimeout(
+        () => {
+          toast.success('دوباره سلام!');
+        }
+        , 0)
+      return { ...state };
 
     case actionTypes.LOGIN_FAILURE:
     case actionTypes.CREATE_ACCOUNT_FAILURE:
-      toast.error('ای بابا! یه مشکلی وجود داره. یه چند لحظه دیگه دوباره امتحان کن.');
+      setTimeout(
+        () => {
+          toast.error('ای بابا! یه مشکلی وجود داره. یه چند لحظه دیگه دوباره امتحان کن.');
+        }
+        , 0)
       return { ...state }
 
     case actionTypes.LOGOUT:
-      toast.info('خدا به همراهت!')
-      return { ...state }
+      setTimeout(
+        () => {
+          toast.info('خدا به همراهت!');
+        }
+        , 0)
+      return { ...state };
 
     default:
       return state;

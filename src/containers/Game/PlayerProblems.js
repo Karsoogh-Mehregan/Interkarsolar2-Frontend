64ef0fe6750f2c778f6bcf6ae845f31742dc2759
amoohashem <amoo.hashem.mehraban@gmail.com>
2021-08-05 06:10:56 +0430

    add "are you sure" dialog

--- src/containers/Game/PlayerProblems.js
+++ src/containers/Game/PlayerProblems.js
@@ -25,7 +25,9 @@ import {
 import { useHistory } from "react-router-dom";
 import { connect } from 'react-redux';
 import { Link, useParams } from 'react-router-dom';
-import { toPersianNumber } from '../../utils/translateNumber'
+import { toPersianNumber } from '../../utils/translateNumber';
+import { toast } from 'react-toastify'
+import AreYouSure from '../../components/Dialog/AreYouSure';
 import {
   getAllSingleProblems,
   getAllMultipleProblems,
@@ -73,6 +75,7 @@ const PlayerProblems = ({
   const history = useHistory();
   const [properties, setProperties] = useState({ difficulty: '', subject: '' });
   const [requestedProblemType, setRequestedProblemType] = useState('single');
+  const [isDialogOpen, setDialogStatus] = useState(false);
   let { gameId } = useParams();
 
 
@@ -90,7 +93,11 @@ const PlayerProblems = ({
     })
   }
 
-  const onClick = (e) => {
+  const getProblem = (e) => {
+    if (!properties.difficulty || !properties.subject) {
+      toast.error('لطفا هم مبحث و هم سختی مسئله‌ی درخواستی را وارد کنید!')
+      return;
+    }
     if (requestedProblemType === 'single') {
       getRandomSingleProblem({ gameId, ...properties })
     } else {
@@ -99,132 +106,139 @@ const PlayerProblems = ({
   }
 
   return (
-    <Container className={classes.container}>
-      <Grid container spacing={2} justify='center'>
-        <Grid item xs={12}>
-          <Typography variant="h1" align="center">«مسئله‌های من»</Typography>
-        </Grid>
-        <Grid item container spacing={2} xs={12} md={10} lg={8}>
-          <Grid item container xs={12} sm={8} direction='column'>
-            <TableContainer component={Paper}>
-              <Table >
-                <TableHead>
-                  <TableRow>
-                    <TableCell>شناسه</TableCell>
-                    <TableCell>عنوان</TableCell>
-                    <TableCell>سختی</TableCell>
-                    <TableCell>وضعیت</TableCell>
-                    <TableCell>نمره</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {singleProblems.map((mySingleProblem, index) =>
-                    <TableRow key={index}>
-                      <TableCell>{toPersianNumber(mySingleProblem.id)}</TableCell>
-                      <TableCell >
-                        <a as={Link} href={'/problem/' + mySingleProblem.id}>{mySingleProblem.problem?.title}</a>
-                      </TableCell>
-                      <TableCell>{DIFFICULTY[mySingleProblem.problem?.difficulty]}</TableCell>
-                      <TableCell>{STATUS[mySingleProblem.status]}</TableCell>
-                      <TableCell>{mySingleProblem.mark == -1 ? '-' : mySingleProblem.mark}</TableCell>
-                    </TableRow>
-                  )}
-                  {multipleProblems.map((myMultipleProblem, index) => {
-                    console.log(myMultipleProblem)
-                    return (
-                      <TableRow key={index}>
-                        <TableCell>{toPersianNumber(myMultipleProblem.id)}</TableCell>
-                        <TableCell >
-                          <a as={Link} href={'/problem/' + myMultipleProblem.id}>{myMultipleProblem.multiple_problem?.title}</a>
-                        </TableCell>
-                        <TableCell>{`${toPersianNumber(myMultipleProblem.multiple_problem?.problems_count || 0)} تایی`}</TableCell>
-                        <TableCell>{`دنباله‌ی ${toPersianNumber(myMultipleProblem.step + 1)}`}</TableCell>
-                        <TableCell>{myMultipleProblem.mark == -1 ? '-' : myMultipleProblem.mark}</TableCell>
-                      </TableRow>
-                    )
-                  }
-                  )}
-                  {singleProblems.length == 0 &&
+    <>
+      <Container className={classes.container}>
+        <Grid container spacing={2} justify='center'>
+          <Grid item xs={12}>
+            <Typography variant="h1" align="center">«مسئله‌های من»</Typography>
+          </Grid>
+          <Grid item container spacing={2} xs={12} md={10} lg={8}>
+            <Grid item container xs={12} sm={8} direction='column'>
+              <TableContainer component={Paper}>
+                <Table >
+                  <TableHead>
                     <TableRow>
-                      <TableCell>هنوز سوال تکی‌ گرفته نشده است!</TableCell>
+                      <TableCell>شناسه</TableCell>
+                      <TableCell>عنوان</TableCell>
+                      <TableCell>سختی</TableCell>
+                      <TableCell>وضعیت</TableCell>
+                      <TableCell>نمره</TableCell>
                     </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {singleProblems.map((mySingleProblem, index) =>
+                      <TableRow key={index}>
+                        <TableCell>{toPersianNumber(mySingleProblem.id)}</TableCell>
+                        <TableCell >
+                          <a as={Link} href={'/problem/' + mySingleProblem.id}>{mySingleProblem.problem?.title}</a>
+                        </TableCell>
+                        <TableCell>{DIFFICULTY[mySingleProblem.problem?.difficulty]}</TableCell>
+                        <TableCell>{STATUS[mySingleProblem.status]}</TableCell>
+                        <TableCell>{mySingleProblem.mark == -1 ? '-' : mySingleProblem.mark}</TableCell>
+                      </TableRow>
+                    )}
+                    {multipleProblems.map((myMultipleProblem, index) => {
+                      console.log(myMultipleProblem)
+                      return (
+                        <TableRow key={index}>
+                          <TableCell>{toPersianNumber(myMultipleProblem.id)}</TableCell>
+                          <TableCell >
+                            <a as={Link} href={'/problem/' + myMultipleProblem.id}>{myMultipleProblem.multiple_problem?.title}</a>
+                          </TableCell>
+                          <TableCell>{`${toPersianNumber(myMultipleProblem.multiple_problem?.problems_count || 0)} تایی`}</TableCell>
+                          <TableCell>{`دنباله‌ی ${toPersianNumber(myMultipleProblem.step + 1)}`}</TableCell>
+                          <TableCell>{myMultipleProblem.mark == -1 ? '-' : myMultipleProblem.mark}</TableCell>
+                        </TableRow>
+                      )
+                    }
+                    )}
+                    {singleProblems.length + multipleProblems.length == 0 &&
+                      <TableRow>
+                        <TableCell>هنوز سوال تکی‌ گرفته نشده است!</TableCell>
+                      </TableRow>
+                    }
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </Grid>
+            <Grid item container xs={12} sm={4}>
+              <Paper className={classes.paper}>
+                <Grid item container direction='column' spacing={2}>
+                  <Grid item>
+                    <Typography variant="h2" align='center'>مسئله‌ی جدید</Typography>
+                  </Grid>
+                  <Divider />
+                  <Grid item>
+                    <FormControl size='small' >
+                      <RadioGroup
+                        row
+                        defaultValue={'single'}
+                        onChange={(e) => setRequestedProblemType(e.target.value)}
+                      >
+                        <FormControlLabel
+                          value="single"
+                          control={<Radio />}
+                          label="تکی"
+                          labelPlacement="end"
+                        />
+                        <FormControlLabel
+                          value="multiple"
+                          control={<Radio />}
+                          label="دنباله‌دار"
+                          labelPlacement="end"
+                        />
+                      </RadioGroup>
+                    </FormControl>
+                  </Grid>
+                  {requestedProblemType == 'single' &&
+                    <>
+                      <Grid item>
+                        <FormControl size='small' variant="outlined" fullWidth>
+                          <InputLabel>مبحث</InputLabel>
+                          <Select
+                            className={classes.dropDown}
+                            onBlur={handleSelect}
+                            name='subject'
+                            label='مبحث'
+                          >
+                            {allSubjects.map((source) => (
+                              <MenuItem value={source.id}>{source.title}</MenuItem>
+                            ))}
+                          </Select>
+                        </FormControl >
+                      </Grid>
+                      <Grid item>
+                        <FormControl size='small' variant="outlined" fullWidth>
+                          <InputLabel>سختی</InputLabel>
+                          <Select
+                            className={classes.dropDown}
+                            onBlur={handleSelect}
+                            name='difficulty'
+                            label='سختی'
+                          >
+                            <MenuItem value={'EASY'}>{'آسان'}</MenuItem>
+                            <MenuItem value={'MEDIUM'}>{'متوسط'}</MenuItem>
+                            <MenuItem value={'HARD'}>{'سخت'}</MenuItem>
+                          </Select>
+                        </FormControl >
+                      </Grid>
+                    </>
                   }
-                </TableBody>
-              </Table>
-            </TableContainer>
+                  <Grid item>
+                    <Button fullWidth variant='contained' color='primary' onClick={() => setDialogStatus(true)}>دریافت</Button>
+                  </Grid>
+                </Grid>
+              </Paper>
+            </Grid>
           </Grid>
-          <Grid item container xs={12} sm={4}>
-            <Paper className={classes.paper}>
-              <Grid item container direction='column' spacing={2}>
-                <Grid item>
-                  <Typography variant="h2" align='center'>مسئله‌ی جدید</Typography>
-                </Grid>
-                <Divider />
-                <Grid item>
-                  <FormControl size='small' >
-                    <RadioGroup
-                      row
-                      defaultValue={'single'}
-                      onChange={(e) => setRequestedProblemType(e.target.value)}
-                    >
-                      <FormControlLabel
-                        value="single"
-                        control={<Radio />}
-                        label="تکی"
-                        labelPlacement="end"
-                      />
-                      <FormControlLabel
-                        value="multiple"
-                        control={<Radio />}
-                        label="دنباله‌دار"
-                        labelPlacement="end"
-                      />
-                    </RadioGroup>
-                  </FormControl>
-                </Grid>
-                {requestedProblemType == 'single' &&
-                  <>
-                    <Grid item>
-                      <FormControl size='small' variant="outlined" fullWidth>
-                        <InputLabel>مبحث</InputLabel>
-                        <Select
-                          className={classes.dropDown}
-                          onBlur={handleSelect}
-                          name='subject'
-                          label='مبحث'
-                        >
-                          {allSubjects.map((source) => (
-                            <MenuItem value={source.id}>{source.title}</MenuItem>
-                          ))}
-                        </Select>
-                      </FormControl >
-                    </Grid>
-                    <Grid item>
-                      <FormControl size='small' variant="outlined" fullWidth>
-                        <InputLabel>سختی</InputLabel>
-                        <Select
-                          className={classes.dropDown}
-                          onBlur={handleSelect}
-                          name='difficulty'
-                          label='سختی'
-                        >
-                          <MenuItem value={'EASY'}>{'آسان'}</MenuItem>
-                          <MenuItem value={'MEDIUM'}>{'متوسط'}</MenuItem>
-                          <MenuItem value={'HARD'}>{'سخت'}</MenuItem>
-                        </Select>
-                      </FormControl >
-                    </Grid>
-                  </>
-                }
-                <Grid item>
-                  <Button fullWidth variant='contained' color='primary' onClick={onClick}>دریافت</Button>
-                </Grid>
-              </Grid>
-            </Paper>
-          </Grid>
-        </Grid>
-      </Grid >
-    </Container>
+        </Grid >
+      </Container>
+      <AreYouSure
+        open={isDialogOpen}
+        handleClose={() => { setDialogStatus(!isDialogOpen) }}
+        callBackFunction={getProblem}
+      />
+    </>
   );
 }
 
